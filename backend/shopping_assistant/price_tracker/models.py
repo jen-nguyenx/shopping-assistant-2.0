3133e9887d446c884e23431df2c82202628f1b9f
Jen Nguyen <jennguyen.datascience@gmail.com>
2025-03-21 23:12:39 +0800

    Create database model for price tracking

--- backend/shopping_assistant/price_tracker/models.py
+++ backend/shopping_assistant/price_tracker/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+    english_name = models.CharField(max_length=200)
+    url = models.URLField()
+
+class HistoricalPrice(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    
