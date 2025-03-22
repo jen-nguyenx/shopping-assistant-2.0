cbd8959ccbe0dea166b00406038b864c3b10fc6a
Jen Nguyen <jennguyen.datascience@gmail.com>
2025-03-22 21:03:46 +0800

    Change project structure

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -122,3 +122,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Celery Configuration
+CELERY_BROKER_URL = 'redis://localhost:6379/0' 
+CELERY_ACCEPT_CONTENT = ['json']
+CELERY_TASK_SERIALIZER = 'json'
---
+++ backend/price_tracker/tasks.py
@@ -0,0 +1,7 @@
+from celery import shared_task
+
+@shared_task
+def test_task():
+    print("Hello Jen")
+    return "Celery is working"
+
