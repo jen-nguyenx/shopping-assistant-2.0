8a199bddbc30c3f73d7f7cf0b0b871a72be635bc
Jen Nguyen <jennguyen.datascience@gmail.com>
2025-04-09 17:16:31 +0800

    Register Product model

--- backend/price_tracker/models.py
+++ backend/price_tracker/models.py
@@ -5,6 +5,9 @@ class Product(models.Model):
     english_name = models.CharField(max_length=200)
     url = models.URLField()
 
+    def __str__(self):
+        return self.english_name
+
 
 class HistoricalPrice(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
