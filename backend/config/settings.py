8a386f6bc248d839d0e072f18bdf1b60a7da4305
Jen Nguyen <jennguyen.datascience@gmail.com>
2025-04-09 16:30:39 +0800

    Add celery beat

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'price_tracker'
+    'price_tracker',
+    'django_celery_beat'
 ]
 
 MIDDLEWARE = [
