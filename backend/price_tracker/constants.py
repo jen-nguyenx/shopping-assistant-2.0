fcac053fe77e3d5e4454953d2bd7b98816db0d04
Jen Nguyen <jennguyen.datascience@gmail.com>
2025-04-27 20:59:34 +0700

    Update brand name links

--- backend/price_tracker/constants.py
+++ backend/price_tracker/constants.py
@@ -3,3 +3,8 @@ SPAN_ID = {
     "full": "p_lt_ctl10_pageplaceholder_p_lt_ctl00_wBR_P_D1_ctl00_ctl00_ctl00_ctl00_ctl04_lblOrignalPriceText",
 }
 SPAN_CLASS = {"discounted": ".product__price", "full": ""}
+
+BRAND_LINK = {
+    "Vitamins": "https://www.chemistwarehouse.com.au/shop-online/81/vitamins", 
+    "Skincare": "https://www.chemistwarehouse.com.au/shop-online/665/skin-care"
+}
